d0395a9ccd816da9c265f1595123e0c3307b22f6
Meghana4c4 <84376992+Meghana4c4@users.noreply.github.com>
2021-06-21 21:06:40 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Array
+{
+    public static void main(String[ ] args)
+   {
+    int n,even=0,odd=0;
+    Scanner s = new Scanner(System.in);
+    System.out.println("Enter array size: ");
+    n=s.nextInt();
+    int a[ ]=new int[n];
+    for (int i=0;i<n;i++)
+     {
+      a[i]=s.nextInt();
+      }
+    for(int i=0;i<n;i++)
+     {
+    if(a[i]%2==0 && i%2==0)
+     {even=even+a[i];}
+    else if (a[i]%2==1 && i%2==1)
+     {odd=odd+a[i];}
+    }
+    System.out.println(even+" "+odd);
+   }
+}
