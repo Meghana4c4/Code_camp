cbac2232066a8262e4ab6f369b401720ed218af1
Meghana4c4 <84376992+Meghana4c4@users.noreply.github.com>
2021-06-23 20:57:52 +0530

    Add files via upload

---
+++ Valley.java
@@ -0,0 +1,96 @@
+
+import java.util.*;
+public class Valley
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+System.out.println("enter size: ");
+int n=s.nextInt();
+int[ ] elements =new int[n];
+System.out.println("Enter "+n+"Elements: ");
+for(int i=0;i<n;i++)
+{
+elements[i]=s.nextInt();
+}
+if(n==0)
+{
+System.out.println("false");
+return;
+}
+int i=1;
+while (i<n)
+{
+if(elements[i-1]>elements[i])
+{
+i=i+1;
+}
+else
+{
+break;
+}
+}
+if((i==n)||(i==1))
+{
+System.out.println("false");
+return;
+}
+while(i<n)
+{
+if(elements[i-1]<elements[i])
+{
+i++;
+}
+else
+{
+break;
+}
+}
+if(i<n)
+{
+System.out.println("false");
+return;
+}
+System.out.println("true");
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
